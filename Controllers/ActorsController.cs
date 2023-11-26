3333643d8819af2d2fcbc5d4a0b30741e7f9eac0
Yass-Bak <baklouti.yessine@iit.ens.tn>
2023-11-26 18:54:08 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,32 @@
+﻿using E_Tickets.Data;
+using E_Tickets.Models;
+using eTickets.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using E_Tickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Tickets.Models
+{
+    public class Movie
+    {
+        [Key]
+       public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+      
+        public string ImageUrl { get; set;}
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+        public List<Actor_Movie> Actors_Movies { get; set; }
+        public int CinemaID { get; set; }
+        [ForeignKey("CinemaID")]
+        public Cinema Cinema { get; set; }
+        public int ProducerID { get; set; }
+        [ForeignKey("ProducerID")]
+        public Producer Producer { get; set; }
+
+
+
+
+    }
+}
