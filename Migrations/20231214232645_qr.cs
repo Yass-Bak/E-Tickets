58826a9d191e24d32b9a248e34d7610472321b04
Yass-Bak <baklouti.yessine@iit.ens.tn>
2023-12-15 00:30:03 +0100

    Qr-Added

---
+++ Migrations/20231214232645_qr.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_Tickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class qr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "QRCodes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "QRCodes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
